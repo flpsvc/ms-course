434aa48e57153dc02eefd06f360e036c25190f45
Felipe Savacini <felipe.savacini@hotmail.com>
2022-05-09 20:39:13 -0300

    ms user

---
+++ hr-config-server-1/src/test/java/com/springms/hrconfigserver/HrConfigServer1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springms.hrconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrConfigServer1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
