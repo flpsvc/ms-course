aa02c893acc03c9f21bd4fd5b5d1510224cc23ca
Felipe Savacini <felipe.savacini@hotmail.com>
2022-05-11 17:47:35 -0300

    ms oauth

---
+++ hr-user-1/src/main/java/com/springms/hroauth/HrUser1Application.java
@@ -0,0 +1,13 @@
+package com.springms.hroauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrUser1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrUser1Application.class, args);
+	}
+
+}
---
+++ hr-user-1/src/test/java/com/springms/hroauth/HrUser1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springms.hroauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUser1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
